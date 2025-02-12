fa636dfd8407bad3a261705793730b03f6ab0b7c
angelalves <angeltrashangel@gmail.com>
2025-02-12 07:04:55 -0300

    feat: finish project

--- src/main/java/com/angel/transacao_api/controller/StatisticsController.java
+++ src/main/java/com/angel/transacao_api/controller/StatisticsController.java
@@ -3,6 +3,9 @@ package com.angel.transacao_api.controller;
 
 import com.angel.transacao_api.business.StatisticsService;
 import com.angel.transacao_api.controller.dtos.StatisticsDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,11 +20,16 @@ public class StatisticsController {
     private final StatisticsService statisticsService;
 
     @GetMapping
-    public ResponseEntity<StatisticsDTO> statisticsCalculate (@RequestParam(defaultValue = 60, required = false, value = "timeInterval") Integer timeInterval) {
+    @Operation(description = "Endpoint responsável por calcular estatisticas das transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Busca efetuada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro na busca de estatísticas de transações"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    public ResponseEntity<StatisticsDTO> statisticsCalculate ( @RequestParam(value = "timeInterval", defaultValue = "60", required = false ) Integer timeInterval) {
 
-//        statisticsService.statisticsCalculate(timeInterval);
 
-        return ResponseEntity.status(HttpStatus.OK).body(statisticsService.statisticsCalculate(timeInterval));
+        return ResponseEntity.ok(statisticsService.statisticsCalculate(timeInterval));
 
     }
 
--- src/main/java/com/angel/transacao_api/controller/TransactionController.java
+++ src/main/java/com/angel/transacao_api/controller/TransactionController.java
@@ -3,6 +3,9 @@ package com.angel.transacao_api.controller;
 
 import com.angel.transacao_api.business.TransactionService;
 import com.angel.transacao_api.controller.dtos.TransactionDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,6 +25,13 @@ public class TransactionController {
     private final TransactionService transactionService;
 
     @PostMapping
+    @Operation(description = "Endpoint responsável por adicionar transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Campos não atendem os requisitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> createTransaction(@RequestBody TransactionDTO dto){
         transactionService.createTransaction(dto);
 
@@ -30,6 +40,12 @@ public class TransactionController {
     }
 
     @DeleteMapping
+    @Operation(description = "Endpoint responsável por deletar todas transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação deletada com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<Void> deleteAllTransactions(){
         transactionService.deleteAllTransactions();
 
